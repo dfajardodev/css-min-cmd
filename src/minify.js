c8d54bd62f054559cbf2e8f7c3ba2d3cd90d8a82
dfajardomcm <44297165+dfajardomcm@users.noreply.github.com>
2021-05-11 22:51:53 -0500

    v1.2.0

--- src/minify.js
+++ src/minify.js
@@ -1,11 +1,8 @@
 module.exports = (str) => {
   str = str.replace(/\/\*(.|\n|\r?\n)*?\*\//g, "");
-  str = str.replace(/\s*(\{|\}|\>|\]|\(|\)|\:|\;|\,)\s*/g, "$1");
+  str = str.replace(/\s*(\{|\}|\>|\]|\(|\:|\;|\,)\s*/g, "$1");
   str = str.replace(/#([\da-fA-F])\1([\da-fA-F])\2([\da-fA-F])\3/g, "#$1$2$3");
-  str = str.replace(
-    /:[\+\-]?0(rem|em|ec|ex|px|pc|pt|vh|vw|vmin|vmax|%|mm|cm|in)/g,
-    ":0"
-  );
+  str = str.replace(/:[\+\-]?0(rem|em|ec|ex|px|pc|pt|vh|vw|vmin|vmax|%|mm|cm|in)/g, ":0");
   str = str.replace(/\r?\n|\r/g, "");
   str = str.replace(/;\}/g, "}");
   str = str.replace(/^\s+|\s+$/g, "");
